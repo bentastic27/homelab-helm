f61a83b4e47307af8d7335c0bd10be1bd3b3e652
Ben Healey <bentastic27@gmail.com>
2019-08-16 11:55:02 -0400

    added rke and init scripts

---
+++ scripts/init-cert-manager.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.9/deploy/manifests/00-crds.yaml
+kubectl create namespace cert-manager
+kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
+  --name cert-manager \
+  --namespace cert-manager \
+  --version v0.9.1 \
+  jetstack/cert-manager
+
+kubectl get pods --namespace cert-manager
---
+++ scripts/init-helm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+kubectl create serviceaccount tiller --namespace=kube-system
+kubectl create clusterrolebinding tiller-admin --serviceaccount=kube-system:tiller --clusterrole=cluster-admin
+helm init --service-account=tiller
+helm repo update
