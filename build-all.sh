280dae666d3edcd6a05623f9b87997bc38be7cc9
Ben Healey <bentastic27@gmail.com>
2019-08-21 14:37:04 -0400

    added build-all script to do everything

---
+++ build-all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [ -f $SCRIPT_DIR/env.config ]; then
+  source $SCRIPT_DIR/env.config
+else
+  echo no env.config, bai
+  exit
+fi
+cd $SCRIPT_DIR
+
+# init helm and cert-manager
+bash scripts/init-helm.sh
+bash scripts/init-cert-manager.sh
+
+# setup cert manager
+helm install charts/cert-manager/ -f answers/cluster-issuer.yml --name cluster-issuer --namespace=cert-manager
+
+# setup sites
+helm install charts/beansnet-sites/ -f answers/pwgen.beansnet.net.yml --name=pwgen-beansnet-net --namespace=sites
+helm install charts/beansnet-sites/ -f answers/beansnet.net.yml --name=beansnet-net --namespace=sites
+helm install charts/beansnet-sites/ -f answers/risk.beansnet.net.yml --name=risk-beansnet-net --namespace=sites
